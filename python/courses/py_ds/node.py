2b51d2bae432eef9627eff2039845024ee5259f9
Mnemosyne <mnemosyne1337@gmail.com>
2024-08-26 21:33:12 -0400

    python node updated class

--- python/courses/py_ds/node.py
+++ python/courses/py_ds/node.py
@@ -1,18 +1,20 @@
-# Create the Node class below:
+#!/usr/bin/env python3
+
 class Node:
 
-  def __init__(self, value, link_node = None):
+  def __init__(self, value, next_node=None):
     self.value = value
-    self.link_node = link_node
-
+    self.next_node = next_node
+  
   def get_value(self):
-      return self.value
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+  
+my_node = Node(44)
 
-  def get_link_node(self):
-      return self.link_node
-
-  def set_value(self, value):
-      self.value = value
-
-  def set_link_node(self, link_node):
-      self.link_node = link_node
+print(my_node.get_value())
