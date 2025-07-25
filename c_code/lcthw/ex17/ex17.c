d17c389c248cf2deea2e4b69d20e32974b248960
mnemosyne12 <mnemosyne1337@gmail.com>
2025-07-25 14:50:49 -0400

    commit to ex18

---
+++ c_code/lcthw/ex17/ex17.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address {
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_DATA];
+};
+
+struct Database {
+	struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
+	FILE* file;
+	struct Database* db;
+};
+
+void die(const char* message) {
+	if (errno) {
+		perror(message);
+	}
+
+	else {
+		printf("ERROR: %s\n", message);
+	}
+
+	exit 1;
+}
+
+void Address_print(struct Address* addr) {
+	printf("%d %s %s\n", addr->id, addr->name, addr->email);
+}
+
+void Database_load(struct Connection* conn) {
+	int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+
+	if (rc !=1) {
+		die("Failed to load database.");
+	}
+}
+
+struct Connection* conn = malloc(sizeof(struct Connection));
+
+if (!conn) {
+	die("Memory Error");
+}
+
+conn->db = malloc(sizeof(struct Database));
+if(!conn->db)
+	die("Memory error");
+
+if (mode == 'c')
