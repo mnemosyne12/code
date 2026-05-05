e9d48412cc3ae9fff64e9cb499c341541e9be113
Mnemosyne Memoriam <mnemosyne1337@gmail.com>
2026-05-05 18:53:03 -0400

    Hello World bwa

---
+++ go/udemy/bwa/basic_app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("start")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello World")
+		if err != nil {
+			fmt.Println("HTTP creation error", err)
+		}
+
+		fmt.Printf("%d bytes written", n)
+	})
+
+	_ = http.ListenAndServe(":8081", nil)
+
+}
