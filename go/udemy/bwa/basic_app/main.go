c7341f9b37a868e414058acab94379a4357a5f2b
Mnemosyne Memoriam <mnemosyne1337@gmail.com>
2026-05-06 16:36:09 -0400

    Added Home and About handler functions

--- go/udemy/bwa/basic_app/main.go
+++ go/udemy/bwa/basic_app/main.go
@@ -7,17 +7,27 @@ import (
 
 const portNumber = ":8081"
 
+// Home handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page") //So this writes back to the response
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(3, 4)
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and the sum is: %d", sum))
+
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
 func main() {
-	fmt.Println("start")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println("HTTP creation error", err)
-		}
 
-		fmt.Printf("%d bytes written", n)
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	_ = http.ListenAndServe(":8081", nil)
+	fmt.Println(fmt.Sprintf("Starting the application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
