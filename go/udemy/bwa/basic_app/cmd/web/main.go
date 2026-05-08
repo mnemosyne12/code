04c8397426e75ffc45d0fc1e7676381e0f04834b
Mnemosyne Memoriam <mnemosyne1337@gmail.com>
2026-05-08 16:46:49 -0400

    Refactoring and additional pkgs

--- go/udemy/bwa/basic_app/cmd/web/main.go
+++ go/udemy/bwa/basic_app/cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/mnemosyne12/code/go/udemy/bwa/basic_app/pkg/handlers"
 )
 
 const portNumber = ":8081"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting the application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- go/udemy/bwa/basic_app/pkg/handlers/handlers.go
+++ go/udemy/bwa/basic_app/pkg/handlers/handlers.go
@@ -1,21 +1,23 @@
-package main
+package handlers
 
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/mnemosyne12/code/go/udemy/bwa/basic_app/pkg/render"
 )
 
 // Home handler
 func Home(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Home handler called.")
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About Handler
 func About(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("About handler called.")
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 
 }
