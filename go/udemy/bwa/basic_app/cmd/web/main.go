f023ceedf16f38c9c8a452c5260ccd189c9fb1da
Mnemosyne Memoriam <mnemosyne1337@gmail.com>
2026-05-09 00:56:49 -0400

    Fixed stupid typo in the about page

--- go/udemy/bwa/basic_app/cmd/web/main.go
+++ go/udemy/bwa/basic_app/cmd/web/main.go
@@ -14,7 +14,7 @@ func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	fmt.Println(fmt.Sprintf("Starting the application on port %s", portNumber))
+	fmt.Printf("Starting the application on port %s\n", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 
 }
--- go/udemy/bwa/basic_app/pkg/render/render.go
+++ go/udemy/bwa/basic_app/pkg/render/render.go
@@ -9,9 +9,11 @@ import (
 // This function renders templates
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-
+	parsedTemplate, err2 := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	if err2 != nil {
+		fmt.Println("Template failed to parse:", err2)
+		return
+	}
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
