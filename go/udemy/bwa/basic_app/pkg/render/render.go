1f6a9827a80b7b7c9f2413c461c81ae30e66078c
Mnemosyne Memoriam <mnemosyne1337@gmail.com>
2026-05-10 15:52:36 -0400

    Refactored the render function

--- go/udemy/bwa/basic_app/pkg/render/render.go
+++ go/udemy/bwa/basic_app/pkg/render/render.go
@@ -3,10 +3,60 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 // This function renders templates
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	//check to see if we already have the template in our cache
+	_, inMap := tc[t]
+
+	if !inMap {
+		//need to create the template
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		//we have the template in the cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	//parse the template
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	//add template to cache
+	tc[t] = tmpl
+	return nil
+}
+
+/*
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, err2 := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
@@ -20,3 +70,4 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		return
 	}
 }
+*/
